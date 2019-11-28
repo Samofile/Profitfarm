e7e478075359124f3aeefda214cf0d2cf8d03dbf
Samofile <samofile@yandex.ru>
2019-11-28 20:54:31 +1000

    Убрал графити на крыше

--- Chickenfarm/Assets/Scripts/Menu.cs
+++ Chickenfarm/Assets/Scripts/Menu.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-
     public GameObject water, eat, clean, take, sell;
-    //public GameObject playerName;
     string a = "Untitle";
     [SerializeField] Text playerName;
+    [SerializeField] GameObject actionPanel;
+    [SerializeField] Text balanceText;
 
-    // Start is called before the first frame update
     void Start()
     {
         a = Login.staticPlayerPhone;
@@ -20,16 +19,24 @@ public class Menu : MonoBehaviour
         //playerName = GameObject.FindGameObjectWithTag("PlayerName");
         //playerName.GetComponent<Text>().text = a.ToString();
         playerName.text = a.ToString();
-
-        
-
         //actionButtons = GameObject.Find("ActionButtons");
+        actionPanel.active = false;
+    }
+
+    private IEnumerator GetBalance(float value)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("playerPhone", Login.staticPlayerPhone);
+        WWW req = new WWW("http://xn--80ajvps.xn--80apnfegdoqc.xn--p1ai/balance.php", form);
+        yield return new WaitForSeconds(value);
+        Debug.Log("Balance: " + req.text);
+        balanceText.text = req.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(GetBalance(5f));
     }
 
     public void ExitPressed()
@@ -37,4 +44,8 @@ public class Menu : MonoBehaviour
         Application.Quit();
     }
     
+    public void Withdrow()
+    {
+        Application.OpenURL("http://xn--b1aghuhmfi.xn--80apnfegdoqc.xn--p1ai/");
+    }
 }
