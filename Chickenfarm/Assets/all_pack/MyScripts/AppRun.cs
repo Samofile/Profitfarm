e56fd266f573c20ba547aff14c4fa203a38dff8c
Samofile <samofile@yandex.ru>
2019-12-09 11:33:04 +1000

    Гравитация яиц

--- Chickenfarm/Assets/all_pack/MyScripts/AppRun.cs
+++ Chickenfarm/Assets/all_pack/MyScripts/AppRun.cs
@@ -51,6 +51,7 @@ public class AppRun : MonoBehaviour
         {
             Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
             GameObject newEgg = Instantiate(myEgg, pos, Quaternion.identity);
+            newEgg.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
             newEgg.SetActive(true);
             eggCount.text = i.ToString();
             yield return new WaitForSeconds(0);
